6671fbc83114912d8f15490045e2a6ae59b45ee9
Scott O'Neil <scottieoneil@gmail.com>
2021-05-07 11:47:47 -0700

    add test header

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text } from 'react-native';
+
+function Header(){
+  return (
+    <>
+      <Text>THIS IS A HEADER YO</Text>
+    </>
+  )
+}
+
+export default Header;
