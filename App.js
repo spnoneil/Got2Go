27f1cb827e6311ca483a45bfa52eaccd0a3c92b8
Scott O'Neil <scottieoneil@gmail.com>
2021-05-07 10:27:42 -0700

    start

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeView from './components/HomeView';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <HomeView />
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
---
+++ components/HomeView.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+class HomeView extends Component {
+    render() {
+        return (
+          <Text>TEST TEXT</Text>
+        );
+    }
+}
+export default HomeView
