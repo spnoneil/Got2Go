73e158616ae59e7f627dc4568546db9553c70001
Scott O'Neil <scottieoneil@gmail.com>
2021-05-14 13:27:37 -0700

    add about pg

---
+++ components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+function About() {
+    return (
+      <View>
+        {/* TODO - about page */}
+        <Text>Got 2 Go will help you go, YADDA YADDA</Text>
+      </View>
+    );
+  }
+
+export default About;
