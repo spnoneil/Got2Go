2e2c4385a6ef6a101f6c0b73b1b0bc7af24837d4
Scott O'Neil <scottieoneil@gmail.com>
2021-05-14 09:30:49 -0700

    add a simple map!

--- components/App.js
+++ components/App.js
@@ -1,15 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 import HomeView from './HomeView';
 import Header from './Header'
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header />
       <HomeView />
-      <Text>Open up App.js to start working on your app!</Text>
+        <MapView
+          style={{height: `50%`, width: `100%`}}
+          provider={PROVIDER_GOOGLE}
+          showUserLocation={true}
+          initialRegion={{
+          latitude: 45.50527180254351,
+          longitude: -122.67506901733977,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421}}
+        />
+      <Text>Open up App.js to start working on your appdfpgdflgkj!</Text>
       <StatusBar style="auto" />
     </View>
   );
