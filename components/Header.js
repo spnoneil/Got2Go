cf073e866f6edb2ffba37f95db60e551bfa00828
Scott O'Neil <scottieoneil@gmail.com>
2021-05-14 10:10:01 -0700

    add logo, test navbar

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,25 @@
 import React from 'react'
-import { Text } from 'react-native';
+import { Text, Image } from 'react-native';
+// import NavigationBar from 'react-native-navbar';
+
+// const rightButtonConfig = {
+//   title: 'Next',
+//   handler: () => alert('hello!'),
+// };
+ 
+// const titleConfig = {
+//   title: 'When you gotta, we got ya!',
+// };
 
 function Header(){
   return (
     <>
-      <Text>THIS IS A HEADER YO</Text>
+      <Image style={{height:`30%`, width: `40%`}} source={require('./../img/logo.png')} />
+      <Text>THIS IS A HEADERrrrr</Text>
+      {/* <NavigationBar
+        title={titleConfig}
+        rightButton={rightButtonConfig}
+      /> */}
     </>
   )
 }
