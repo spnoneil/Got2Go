35a8391c6df7316d472bf5d3731d18c90deec1ff
Scott O'Neil <scottieoneil@gmail.com>
2021-05-14 10:39:44 -0700

    navbar testing

--- components/App.js
+++ components/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import HomeView from './HomeView';
 import Header from './Header'
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
@@ -13,23 +13,24 @@ export default function App() {
         <MapView
           style={{height: `50%`, width: `100%`}}
           provider={PROVIDER_GOOGLE}
-          showUserLocation={true}
+          showsUserLocation={true}
           initialRegion={{
           latitude: 45.50527180254351,
           longitude: -122.67506901733977,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421}}
         />
-      <Text>Open up App.js to start working on your appdfpgdflgkj!</Text>
+      <Text>Open up App.js to start working on your lifeasdfsd!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#5FB7EE',
     alignItems: 'center',
     justifyContent: 'center',
   },
--- components/Header.js
+++ components/Header.js
@@ -1,25 +1,25 @@
 import React from 'react'
 import { Text, Image } from 'react-native';
-// import NavigationBar from 'react-native-navbar';
+import NavigationBar from 'react-native-navbar';
 
-// const rightButtonConfig = {
-//   title: 'Next',
-//   handler: () => alert('hello!'),
-// };
+const rightButtonConfig = {
+  title: <Text style={{flex: 2}}>New Bathroom</Text>,
+  handler: () => alert('hello!'),
+};
  
-// const titleConfig = {
-//   title: 'When you gotta, we got ya!',
-// };
+const titleConfig = {
+  title: <Text style={{ flex: 1 }}>When you gotta, we got ya!</Text>,
+};
 
 function Header(){
   return (
     <>
       <Image style={{height:`30%`, width: `40%`}} source={require('./../img/logo.png')} />
-      <Text>THIS IS A HEADERrrrr</Text>
-      {/* <NavigationBar
+      <NavigationBar
+        style={{height:`10%`, width: `90%`}}
         title={titleConfig}
         rightButton={rightButtonConfig}
-      /> */}
+      />
     </>
   )
 }
